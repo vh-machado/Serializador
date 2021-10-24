4820b2ace288a5a166694e1a741196c236ff2a41
vh-machado <vh.machado.silva@gmail.com>
2021-10-24 11:00:41 -0300

    Inicializando projeto

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  
+  const [seriesDom, setSeriesDom] = useState([
+    {id: 0, serie: 'Evil'},
+  ]);
+  const [seriesSeg, setSeriesSeg] = useState([
+    {id: 0, serie: 'Black Lightning'},
+  ]);
+  const [seriesTer, setSeriesTer] = useState([
+    {id: 0, serie: 'Stargirl'},
+    {id: 1, serie: 'Chucky'},
+  ]);
+  const [seriesQua, setSeriesQua] = useState([
+    {id: 0, serie: 'Legends of Tomorrow'},
+    {id: 1, serie: 'The Wonder Years (2021)'},
+  ]);
+  const [seriesQui, setSeriesQui] = useState([
+    {id: 0, serie: 'Doom Patrol'},
+    {id: 1, serie: 'RuPauls Drag Race UK'},
+    {id: 2, serie: 'Canadas Drag Race'},
+  ]);
+  const [seriesSex, setSeriesSex] = useState([
+    {id: 0, serie: 'Foundation'},
+    {id: 1, serie: 'Ted Lasso'}
+  ]);
+  const [seriesSab, setSeriesSab] = useState([
+    {id: 0, serie: 'Harley Queen'},
+  ]);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.scroll}>
+      <View style={styles.container}>
+        
+        <StatusBar style='auto' backgroundColor='#c2ffe1'/>
+        
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Domingo</Text>
+          {seriesDom.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Segunda</Text>
+          {seriesSeg.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Terça</Text>
+          {seriesTer.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Quarta</Text>
+          {seriesQua.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Quinta</Text>
+          {seriesQui.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Sexta</Text>
+          {seriesSex.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Sábado</Text>
+          {seriesSab.map(function(val){
+            return(<Text>{val.serie}</Text>);
+          })}
+        </View>
+
+      </View>
+    </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
+  scroll: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    marginTop: Constants.statusBarHeight,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingEnd: 10,
+    paddingStart: 10,
+    paddingBottom: 10,
+    paddingTop: 10,
+    backgroundColor: '#c2ffe1',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  card: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 15,
+    width: '100%',
+    borderRadius: 10,
+    backgroundColor: '#bdf28f',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  cardTitle: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 5,
   },
 });
